cf3495691739c9771dc48eff33f981cf7d1f377e
Grachik Tsatsuryan <hrachtsaturian@gmail.com>
2024-09-26 19:06:25 -0700

    react routers patterns apps

---
+++ Color Factory App/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from "react-router-dom";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
